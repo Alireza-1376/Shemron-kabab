eb1a4eeaead94fabedf9273cce6cdf45c0d8507a
alireza <alirezahabibi1376118@gmail.com>
2025-10-02 14:33:18 +0330

    shemron-kabab part1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css"
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-thin text-3xl">ابوالفضل</h1>
+      <h1 className="font-extralight text-3xl">ابوالفضل</h1>
+      <h1 className="font-light text-3xl">ابوالفضل</h1>
+      <h1 className="font-normal text-3xl">ابوالفضل</h1>
+      <h1 className="font-medium text-3xl">ابوالفضل</h1>
+      <h1 className="font-semibold text-3xl">ابوالفضل</h1>
+      <h1 className="font-bold text-3xl">ابوالفضل</h1>
+    </div>
+  );
+}
+
+export default App;
